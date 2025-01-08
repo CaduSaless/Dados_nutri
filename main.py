5495934da725b85e54279537dba7385f7366c21a
Cadu__sales <cadubss2@gmail.com>
2025-01-07 22:08:20 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from routes.home import home_route
+from routes.dados import dados_route
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Cadu'
+
+app.register_blueprint(home_route)
+app.register_blueprint(dados_route)
+
+app.run(debug=True)
